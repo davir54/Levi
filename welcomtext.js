8850983d06ee83080d92b1ffe1e4d80c91336828
davir54 <82084613+davir54@users.noreply.github.com>
2021-06-17 07:52:12 -0300

    Delete welcomtext.js

--- welcomtext.js
+++
@@ -1,9 +0,0 @@
-const welcome = (number, groupname) => {
-    return `𝐎𝐥𝐚 @${number}. 𝐒𝐞𝐣𝐚 𝐛𝐞𝐦-𝐯𝐢𝐧𝐝𝐨 𝐚𝐨 𝐠𝐫𝐮𝐩𝐨 ${groupname}`
-}
-exports.welcome = welcome
-
-const bye = (number) => {
-    return `Adeus @${number}. Já vai tarde 👋`
-}
-exports.bye = bye
